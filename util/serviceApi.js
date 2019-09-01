49e382a717536346b0c9b69e986f59ec651eb201
douira <douira100@gmail.com>
2019-09-01 23:08:22 +0200

    added another api step, but the api is currently down

--- util/serviceApi.js
+++ util/serviceApi.js
@@ -15,16 +15,36 @@ const time = parseInt(address.housenumber, 10)
 //queries the service api for one address
 export const getAddressStatus = async address => {
   //get address id for given address from api
-  const { data } = await serviceApiInstance.post(
-    "gimli/customer/addressvalidation",
-    {
-      //the address data
-      zipcode: address.postcode,
-      street: address.street,
-      housenumber: address.housenumber
+  const {
+    data: {
+      data: { addresses }
     }
-  )
-  console.log(data)
+  } = await serviceApiInstance.post("gimli/customer/addressvalidation", {
+    //the address data
+    zipcode: address.postcode,
+    street: address.street,
+    housenumber: address.housenumber
+  })
+
+  //attach the api id of the first gotten address
+  address.apiId = addresses[0].addressId
+
+  //set the address id in the current api state
+  const {
+    data: {
+      data: { valid }
+    }
+  } = await serviceApiInstance.post("gimli/customer/addressvalidation", {
+    //pass the gotten address id
+    addressId: address.apiId
+  })
+
+  //throw to abort if not valid
+  if (!valid) {
+    throw Error("api address id not accepted by api")
+  }
+
+  console.log(addresses)
   //set address id in the api
   //get DSL products for address
   //get cable marketing data for address
