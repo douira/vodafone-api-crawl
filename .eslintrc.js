3845326bfc0b5571af2ccee12c496c3d89dee472
douira <douira100@gmail.com>
2019-12-31 13:21:47 +0100

    package updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   globals: {
     $nuxt: true
   },
+  ignorePatterns: ["!.prettierrc", "!.eslintrc.js"],
   plugins: ["sonarjs"],
   extends: [
     "plugin:sonarjs/recommended",
@@ -22,15 +23,19 @@ module.exports = {
     "plugin:prettier/recommended"
   ],
   rules: {
-    "no-console":
-      process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger":
-      process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/no-v-html": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-constant-condition": ["error", { checkLoops: false }],
     "import/no-unresolved": "off",
     "promise/catch-or-return": "off",
     "promise/always-return": "off",
-    "sonarjs/cognitive-complexity": ["warn", 15]
+    "no-var": "error",
+    "sonarjs/cognitive-complexity": ["warn", 15],
+    "vue/no-v-html": "off",
+    radix: "error",
+    "max-len": [
+      "error",
+      { code: Infinity, comments: 100, ignorePattern: "TODO|DEBUG|INFO" }
+    ]
   }
 }
